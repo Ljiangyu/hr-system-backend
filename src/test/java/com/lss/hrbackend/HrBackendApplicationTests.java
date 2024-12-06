a946f981b1c46683c0886e1196b3c8c61ebc8988
Ljiangyu <104154611+Ljiangyu@users.noreply.github.com>
2024-12-06 11:11:35 +0800

    init

---
+++ src/test/java/com/lss/hrbackend/HrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lss.hrbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
